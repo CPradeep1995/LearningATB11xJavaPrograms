771e4146280ad4e4b1171477d07b07a45c0e2cc2
CPradeep1995 <pradeepchowlur@gmail.com>
2025-03-06 08:36:03 +0530

    First Program had error and changed few settings to add it to GIT

---
+++ src/ex_01_java_Program/Lab0001_HelloWorldProgram.java
@@ -0,0 +1,12 @@
+package ex_01_java_Program;
+
+public class Lab0001_HelloWorldProgram {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+
+
+    }
+
+}
